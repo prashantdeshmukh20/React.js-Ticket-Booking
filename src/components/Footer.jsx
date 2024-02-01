39de51e0a485241fbf4e72d7115bab66326cdc49
Prashant Deshmukh <deshmukhprashant2205@gmail.com>
2024-02-01 20:51:30 +0530

    Inicial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+function Footer() {
+  return (
+    <React.Fragment>
+     <div className="footer"><hr/>&#9400;<a target="_blank" href='http://www.github.com/prashantdeshmukh20'>Prashant Deshmukh</a></div>
+     </React.Fragment>
+  )
+}
+
+export default Footer;
